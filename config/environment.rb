f902fbe22127d4cfaca17eb16f5e4f4894a29fe0
Adam Thompson <ado@ucsc.edu>
2008-09-03 17:26:09 -0700

    updated hpricot and ferret versions

--- config/environment.rb
+++ config/environment.rb
@@ -25,8 +25,8 @@ Rails::Initializer.run do |config|
   config.gem 'youtube-g', :version=> '0.4.1', :lib=>'youtube_g'
   config.gem 'uuidtools', :version=> '1.0.3'
   config.gem 'acts_as_ferret', :version=> '0.4.3'
-  config.gem 'ferret', :version=> '0.11.4'
-  config.gem 'hpricot', :version=>"0.6"
+  config.gem 'ferret', :version=> '0.11.6' # not included in build
+  config.gem 'hpricot', :version=>"0.6.161" # not inlcuded in build
   config.gem 'mocha', :version=>"0.5.6"
   config.gem 'redgreen', :version=>"1.2.2"
   config.gem 'gcnovus-avatar', :version=>"0.0.7", :lib => 'avatar'
