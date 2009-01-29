4d339885cac1205d7267c1ee7fdc584f26fccfb1
Adam Thompson <ado@ucsc.edu>
2009-01-28 16:57:32 -0800

    changed email for exception notifier

--- config/initializers/exception_notifier.rb
+++ config/initializers/exception_notifier.rb
@@ -1,5 +1,5 @@
 # Add the emails which should receive exception notifications below.  ie: %W(admin1@yoursite.com admin2@yoursite.com)
 
-ExceptionNotifier.sender_address =  %("CA Voices Error <error@californiavoices.org>")
-ExceptionNotifier.exception_recipients = %W(adam@giip.org)
+ExceptionNotifier.sender_address =  %("CA Voices Error <info@californiavoices.org>")
+ExceptionNotifier.exception_recipients = %W(error@californiavoices.org)
 ExceptionNotifier.email_prefix = "[California Voices Exception] "
