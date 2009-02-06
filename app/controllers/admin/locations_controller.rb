e46d807f6fba0d597bd9b9793b27f8128e335fdf
Adam Thompson <ado@ucsc.edu>
2009-02-05 20:47:21 -0800

    add new location form and link

--- app/controllers/admin/locations_controller.rb
+++ app/controllers/admin/locations_controller.rb
@@ -4,6 +4,26 @@ class Admin::LocationsController < ApplicationController
     @results = Location.find(:all, :order => "region_id ASC, created_at DESC").paginate(:page => @page, :per_page => @per_page)
   end
 
+  def new
+    @location = Location.new
+  end
+
+  def create
+    @location = Location.new(params[:location])
+    respond_to do |wants|
+      if @location.save
+        flash[:notice] = "Location was successfully created."
+        wants.html { redirect_to(admin_locations_path) }
+        wants.xml { head :created, :location => admin_locations_path }
+      else
+        flash[:error] = "Location could NOT be created."
+        wants.html { render :action => 'new' }
+        wants.xml { render :xml => @location.errors.to_xml }
+      end
+    end
+    
+  end
+  
   def edit
     @location = Location.find params[:id]
   end
