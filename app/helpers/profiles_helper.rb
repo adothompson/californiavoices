48594aed5243a2a0bfb566b1c702fd7b049fc996
Adam Thompson <ado@ucsc.edu>
2009-02-23 12:27:24 -0800

    fixed profile error in location link

--- app/helpers/profiles_helper.rb
+++ app/helpers/profiles_helper.rb
@@ -8,7 +8,8 @@ module ProfilesHelper
     img_opts = {:title => profile.full_name, :alt => profile.full_name, :class => size}.merge(img_opts)
     link_to(avatar_tag(profile, {:size => size, :file_column_version => size }, img_opts), profile_path(profile)) rescue ''
   end
-  
+
+  # TODO: link this to location page
   def location_link profile = @p
     return profile.location_name if profile.location_name == Profile::NOWHERE
     link_to h(profile.location_name), search_profiles_path.add_param('search[location_name]' => profile.location_name)
